85ea7ffc81126b4e4a9941a98c6c45ce3c3e0b05
Ybclock Operator <vvybclock@gmail.com>
2020-10-16 11:34:31 -0400

    Move over P7888 code from labscriptlib git. Commit #7151e24ec5a6c715a3f85e25a967f438599558fe

---
+++ P7888/P7888.py
@@ -0,0 +1,11 @@
+#Labscript API File
+
+###########################################################################
+#		Written by Enrique Mendez (eqm@mit.edu)	c. 2020	
+###########################################################################
+
+class P7888(object):
+	"""docstring for P7888"""
+	def __init__(self, arg):
+		super(P7888, self).__init__()
+		self.arg = arg
---
+++ P7888/register_classes.py
@@ -0,0 +1,6 @@
+from labscript_devices import register_classes
+
+register_classes(
+	'P7888',
+	
+)
