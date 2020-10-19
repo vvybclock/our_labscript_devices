7f4ea84f62f6e0b8cfb60568481d061d8bf6dd3f
Enrique Mendez <eqm@mit.edu>
2020-10-19 13:52:13 -0400

    Rename blacs_gui to blacs_tab

---
+++ P7888/blacs_tabs.py
@@ -0,0 +1,27 @@
+###########################################################################
+#		Written by Enrique Mendez (eqm@mit.edu)	c. 2020	
+###########################################################################
+#To see how this file is accessed by labscript see register_classes.py
+
+from blacs.device_base_class import DeviceTab
+
+class P7888_Tab(DeviceTab):
+	
+	def initialise_workers(self):
+		#Does spelling of the function name matter? (-ize vs -ise?)
+
+		#Look up Connection Settings from the Connection Table
+		connection_table = self.settings['connection_table']
+		device = connection_table.find_by_name(self.device_name)
+
+		#pull particular settings needed here.
+		particular_setting = device.properties['particular_setting_identifier']
+
+		#Start the worker process associated with our worker class.
+		#Pass it the particular settings it needs to connect.
+		self.create_worker(
+			'main_worker',
+			'user_devices.P7888.blacs_workers.P7888_Worker'
+		)
+
+		pass
