25118f659a2af5e61a924da676dafbd5f6ccb07e
Ybclock Operator <vvybclock@gmail.com>
2020-10-21 14:10:49 -0400

    Attempt implementation of p7888_dll into worker.

--- P7888/blacs_worker.py
+++ P7888/blacs_worker.py
@@ -4,6 +4,7 @@
 #To see how this file is accessed by labscript see register_classes.py
 
 #Add in libraries for communicating with the device
+import user_devices.P7888.p7888_dll
 
 #Add in libraries for working with HDF files
 import labscript_utils.h5_lock
@@ -13,28 +14,82 @@ import h5py
 from blacs.tab_base_classes import Worker
 
 
+
 class TemplateWorker(Worker):
 	def init(self):
+		#Initialization code called once when BLACS is started. Can be used to
+		#initialize the device in question and see if it's turned on, etc.
+
+		pass
+
+	def shutdown(self):
+		#Called once when BLACS exits.
+
 		pass
 
 	def program_manual(self,values):
-		pass
+		#talk to the device in question whenever a value in the GUI changes to write
+		#those values in the device.
+
+		return {}
 
 	def transition_to_buffered(self, device_name, h5_file, initial_values, fresh):
-		pass
+		# - Access the shot file for getting hardware instructions encoded in the compilation process.
+		# - Write those instructions to the device.
+		# - If needed, set the device to respond to hw/sw triggers.
+		# - Use initial_values to set the device to ensure continuity.
+		# - The fresh variable specifies whether the entire table of instructions should be reprogrammed.
+		#	- Only useful if the device supports partial programming.
+		# - Return final_values. A dict holding the last values of the sequence. This allows blacs to retain
+		# - output continuity after the shot is finished. 
+		
+		return final_values
 
 	def transition_to_manual(self):
+		# - Called after shot is finished.
+		# - The device should be placed in manual mode here.
+		# - Useful for saving data.
+		# - Return True on success.
+		
+		return True
+
+	def abort_buffered(self):
+		# Called only if transition_to_buffered succeeded and the # shot if aborted prior to the initial trigger
+		# return True on success
+		
+		return True
+
+	def abort_transition_to_buffered(self):
+		# Called only if transition_to_buffered succeeded and the # shot if aborted prior to the initial trigger
+		# return True on success
+
+		return True
+
+class P7888_Worker(Worker):
+	def init(self):
+		#define variable placeholders for the worker.
+		self.shot_file = None
+
+
 		pass
+
+	def program_manual(self,values):
+		return {}
+
+	def transition_to_buffered(self, device_name, h5_file, initial_values, fresh):
+		# - Set the Device to respond to hardware triggers.
+		p7888_dll.Start()
+		return {}
+
+	def transition_to_manual(self):
+		p7888_dll.Halt()
+		return True
 
 	def shutdown(self):
 		pass
 
 	def abort_buffered(self):
-		pass
+		return True
 
 	def abort_transition_to_buffered(self):
-		pass
-
-class P7888_Worker(Worker):
-	def init(self):
-		pass
+		return True
