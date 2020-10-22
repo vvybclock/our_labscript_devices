222176122bf7cf96d1f46eccc8e8153f30a247ff
Ybclock Operator <vvybclock@gmail.com>
2020-10-22 12:43:37 -0400

    Remove write empty_file check

--- P7888/register_classes.py
+++ P7888/register_classes.py
@@ -30,9 +30,6 @@ from labscript_devices import register_classes
 # things to have.
 # Taken from https://github.com/labscript-suite/labscript-utils/blob/master/labscript_utils/device_registry/_device_registry.py
 
-with open(r'C:\Users\Boris\labscript-suite\userlib\user_devices\P7888\_register_class_ran.txt', 'w') as fp:
-	pass
-
 register_classes(
 	'P7888',
 	BLACS_tab = 'user_devices.P7888.blacs_tabs.P7888Tab',
